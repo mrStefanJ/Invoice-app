fa8d977c88237b6bf284ab5ef541361995769ea2
Stefan <stefan.joncic@united.group>
2022-05-27 16:40:22 +0200

    add material module

---
+++ app/material module/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+    imports:[MatToolbarModule,
+        MatSlideToggleModule,
+        MatCardModule,
+        MatButtonModule,
+        MatSidenavModule],
+    exports: [MatToolbarModule,
+        MatSlideToggleModule,
+        MatCardModule,
+        MatButtonModule,
+        MatSidenavModule]
+})
+
+export class MaterialModule {}
