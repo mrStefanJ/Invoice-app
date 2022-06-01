7816432646008a63771963b8ffc01fdc300fc336
Stefan <stefan.joncic@united.group>
2022-06-01 14:52:58 +0200

    firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCl66M6VO4vxjpzGzGCgQH3zQPmtScitPE",
+    authDomain: "auth-user-25df3.firebaseapp.com",
+    databaseURL: "https://auth-user-25df3-default-rtdb.firebaseio.com",
+    projectId: "auth-user-25df3",
+    storageBucket: "auth-user-25df3.appspot.com",
+    messagingSenderId: "74452231501",
+    appId: "1:74452231501:web:af67a83c50cf89b642c304",
+    measurementId: "G-0ZFB2L9P3T"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCl66M6VO4vxjpzGzGCgQH3zQPmtScitPE",
+    authDomain: "auth-user-25df3.firebaseapp.com",
+    databaseURL: "https://auth-user-25df3-default-rtdb.firebaseio.com",
+    projectId: "auth-user-25df3",
+    storageBucket: "auth-user-25df3.appspot.com",
+    messagingSenderId: "74452231501",
+    appId: "1:74452231501:web:af67a83c50cf89b642c304",
+    measurementId: "G-0ZFB2L9P3T"
+  }
 };
 
 /*
