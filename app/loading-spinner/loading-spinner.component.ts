915177d03bfaa2218d9c7fb09a05431842ffd4f9
Stefan <stefan.joncic@united.group>
2022-06-01 14:43:58 +0200

    delete folder

--- app/loading-spinner/loading-spinner.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-loading-spinner',
-    templateUrl: './loading-spinner.component.html'
-})
-
-export class LoadingSpinnerComponent {}
