035d8b256ea5623870fb6036fda9edc59a4ae0f3
Stefan <stefan.joncic@united.group>
2022-06-03 16:42:02 +0200

    adding new item and error message

--- src/app/component/add-item/add-item.component.ts
+++ src/app/component/add-item/add-item.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { MatSidenav } from '@angular/material/sidenav';
-import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TableModel } from 'src/app/model/table.models';
 import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
@@ -13,41 +12,40 @@ import { EmployeeService } from 'src/app/service/employee.service';
 export class AddItemComponent implements OnInit {
   @Input() inputSideNav!: MatSidenav;
   submitted = false;
-  invoiceStatus = 'Pending';
-  showErrorMessage = false;
 
   invoiceForm = this.fb.group(
     {
       senderAddress: this.fb.group({
         street: ['', Validators.required],
         city: ['', Validators.required],
-        postCode: ['', Validators.required],
+        postCode: ['', [Validators.required, Validators.maxLength(5)]],
         country: ['', Validators.required],
       }),
       clientName: ['', Validators.required],
-      clientEmail: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      // clientAddress: this.fb.group({
-      //   street: ['', Validators.required],
-      //   city: ['', Validators.required],
-      //   postCode: ['', Validators.required],
-      //   country: ['', Validators.required],
-      // }),
-      //paymentDue: ['', Validators.required],
-      // paymentTerms: ['', Validators.required],
+      clientEmail: ['', [Validators.required, Validators.email]],
+      clientAddress: this.fb.group({
+         street: ['', Validators.required],
+         city: ['', Validators.required],
+         postCode: ['', [Validators.required, Validators.maxLength(5)]],
+         country: ['', Validators.required],
+       }),
+      paymentDue: ['', Validators.required],
+      //paymentTerms: ['', Validators.required],
       //description: ['', Validators.required],
       //invoiceItemList: this.fb.array([], Validators.required)
     },
-    { updateOn: 'submit' }
-  );
+    { 
+      updateOn: 'submit' 
+    });
 
   constructor(
     private fb: FormBuilder,
     private employeeService: EmployeeService,
-    private router: Router,
     private toastr: ToastrService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   public closeSidenav() {
     return this.inputSideNav.close();
@@ -58,8 +56,6 @@ export class AddItemComponent implements OnInit {
     if (this.invoiceForm.invalid) {
       return;
     }
-    console.log(this.invoiceForm.value.senderAddress)
-    console.log(this.invoiceForm.value.senderAddress.street)
     const employee: any = {
       street: this.invoiceForm.value.senderAddress.street,
       city: this.invoiceForm.value.senderAddress.city,
@@ -67,32 +63,26 @@ export class AddItemComponent implements OnInit {
       country: this.invoiceForm.value.senderAddress.country,
       clientName: this.invoiceForm.value.clientName,
       clientEmail: this.invoiceForm.value.clientEmail,
-      // clientAddress: this.invoiceForm.value.clientAddress.street,
-      // clientCity: this.invoiceForm.value.clientAddress.city,
-      // clientPostCode: this.invoiceForm.value.clientAddress.postCode,
-      // clientCountry: this.invoiceForm.value.clientAddress.country,
-      // paymentDue: new Date(),
-      // paymentTerms: this.invoiceForm.value.paymentTerms,
-      // project: this.invoiceForm.value.description,
+      clientStreet: this.invoiceForm.value.clientAddress.street,
+      clientCity: this.invoiceForm.value.clientAddress.city,
+      clientPostCode: this.invoiceForm.value.clientAddress.postCode,
+      clientCountry: this.invoiceForm.value.clientAddress.country,
+      paymentDue: this.invoiceForm.value.paymentDue,
+      //paymentTerms: this.invoiceForm.value.paymentTerms,
+      //description: this.invoiceForm.value.description,
     };
-    console.log(this.invoiceForm.value);
-    console.log(employee)
-    this.employeeService
-      .createNewInvoice(employee)
-      .then(() => {
+
+    this.employeeService.createNewInvoice(employee)
+    .then((data) => {
+        console.log(data);
         this.toastr.success('Successfully added', 'Employee Invoices', {
           positionClass: 'toast-top-right'
         });
-        console.log(employee);
         this.invoiceForm.reset();
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
         this.toastr.error('Error added', 'Employee Invoices', {positionClass: 'toast-top-right'})
-      });
-  }
-
-  get emailClient(){
-        return this.invoiceForm.get('clientEmail');
+      })
   }
 }
